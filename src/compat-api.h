490eba84881ae95d887e1c06078dce1dab45099a
Arthur Demchenkov <spinal.by@gmail.com>
2019-03-02 02:47:59 +0300

    Adapt Block/WakeupHandler signature for ABI 23

--- src/compat-api.h
+++ src/compat-api.h
@@ -80,8 +80,13 @@
 
 #define SCREEN_INIT_ARGS_DECL ScreenPtr pScreen, int argc, char **argv
 
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 23
 #define BLOCKHANDLER_ARGS_DECL ScreenPtr arg, pointer pTimeout, pointer pReadmask
 #define BLOCKHANDLER_ARGS arg, pTimeout, pReadmask
+#else
+#define BLOCKHANDLER_ARGS_DECL ScreenPtr arg, pointer pTimeout
+#define BLOCKHANDLER_ARGS arg, pTimeout
+#endif
 
 #define CLOSE_SCREEN_ARGS_DECL ScreenPtr pScreen
 #define CLOSE_SCREEN_ARGS pScreen
